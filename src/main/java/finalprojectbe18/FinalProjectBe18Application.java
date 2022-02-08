71f1b541e99b40cf9cf796dfe442ae6814f447ea
Hadiwasito1905 <noverryambo@gmail.com>
2022-02-08 10:15:29 +0700

    final project be18

---
+++ src/main/java/finalprojectbe18/FinalProjectBe18Application.java
@@ -0,0 +1,13 @@
+package finalprojectbe18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectBe18Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectBe18Application.class, args);
+    }
+
+}
---
+++ src/test/java/finalprojectbe18/FinalProjectBe18ApplicationTests.java
@@ -0,0 +1,13 @@
+package finalprojectbe18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectBe18ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
